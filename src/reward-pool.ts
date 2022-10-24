c37ba78c22afaade9855c6c11a8e9d9985ea59bc
Tuyen Nguyen <tuyen.nguyen@sotatek.com>
2022-10-24 16:40:09 +0700

    Add subgraph

--- src/reward-pool.ts
+++ src/reward-pool.ts
@@ -1,3 +1,4 @@
+import { SetNoWithdrawFeeTimeCall__Inputs } from './../generated/Reward_pool/Reward_pool';
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   Reward_pool,
@@ -17,75 +18,27 @@ import {
   UpdateAdmin,
   Withdraw
 } from "../generated/Reward_pool/Reward_pool"
-import { ExampleEntity } from "../generated/schema"
+import { DepositEntity, WithdrawEntity } from "../generated/schema"
 
 export function handleAdminTokenRecovery(event: AdminTokenRecovery): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.tokenRecovered = event.params.tokenRecovered
-  entity.amount = event.params.amount
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.Admin(...)
-  // - contract.PRECISION_FACTOR(...)
-  // - contract.PRECISION_FACTOR_FE(...)
-  // - contract.aNumber(...)
-  // - contract.admin(...)
-  // - contract.bNumber(...)
-  // - contract.cNumber(...)
-  // - contract.calculateAPR(...)
-  // - contract.calculateWithdrawFee(...)
-  // - contract.currentTotalStaked(...)
-  // - contract.fixedAPR(...)
-  // - contract.isEmergency(...)
-  // - contract.maxStake(...)
-  // - contract.maxTotalStake(...)
-  // - contract.minStake(...)
-  // - contract.noWithdrawFeeTime(...)
-  // - contract.owner(...)
-  // - contract.penaltyWallet(...)
-  // - contract.pendingReward(...)
-  // - contract.rewardDistributionAddress(...)
-  // - contract.userInfo(...)
 }
 
-export function handleChangeToken(event: ChangeToken): void {}
+export function handleChangeToken(event: ChangeToken): void {
+
+}
 
 export function handleClaim(event: Claim): void {}
 
-export function handleDeposit(event: Deposit): void {}
+export function handleDeposit(event: Deposit): void {
+  const depositEntity = new DepositEntity(event.transaction.hash.toHex())
+
+  depositEntity.id = event.transaction.hash.toHex();
+  depositEntity.user = event.params._user;
+  depositEntity.amount = event.params.amount;
+  depositEntity.time = event.block.timestamp;
+
+  depositEntity.save();
+}
 
 export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {}
 
@@ -109,4 +62,13 @@ export function handleSetWithdrawTime(event: SetWithdrawTime): void {}
 
 export function handleUpdateAdmin(event: UpdateAdmin): void {}
 
-export function handleWithdraw(event: Withdraw): void {}
+export function handleWithdraw(event: Withdraw): void {
+  const withdrawEntity = new WithdrawEntity(event.transaction.hash.toHex());
+
+  withdrawEntity.id = event.transaction.hash.toHex();
+  withdrawEntity.user = event.params._user;
+  withdrawEntity.amount = event.params.amount;
+  withdrawEntity.time = event.block.timestamp;
+
+  withdrawEntity.save(); 
+}
